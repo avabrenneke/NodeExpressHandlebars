11c82fbde6a948b50715fe9c01cf014f7579bd06
ava brenneke <avabrenneke@gmail.com>
2021-01-23 18:59:12 -0800

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Double");
+INSERT INTO burgers (burger_name) VALUES ("Animal Style");
+INSERT INTO burgers (burger_name) VALUES ("Protein Style");
