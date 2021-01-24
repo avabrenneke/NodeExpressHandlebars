1b2a9d98d27097a226033bdd93fb349684fe0997
ava brenneke <avabrenneke@gmail.com>
2021-01-23 19:42:05 -0800

    created server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");   
+var exphbs = require("express-handlebars");                      
+var bodyParser = require("body-parser");                            
+var routes = require("./controllers/burgers_controller.js");      
+var app = express();                                           
+var PORT = process.env.PORT || 4040;                              
+
+//public folder
+app.use(express.static("public"));                                
+
+//parses JSON
+app.use(bodyParser.json());                                         
+app.use(bodyParser.urlencoded({ extended: true }));                 
+
+
+app.engine("handlebars", exphbs({                                 
+    defaultLayout: "main"                                        
+}));
+
+app.set("view engine", "handlebars");                               
+
+// API CALL
+app.use(routes);                                                 
+app.listen(PORT, function(){                             
+console.log("Listening on Port: " + PORT);
+});
